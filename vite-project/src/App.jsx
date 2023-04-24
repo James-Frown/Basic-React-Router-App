a6d351b90f8a6696be23b1bc2b808a6a58bf4fcd
James-Frown <21122@ctca.co.za>
2023-04-24 21:32:55 +0200

    Updating Documentation & Changes to the overall file, editied out all of the react dom intems then added more pages

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
+{/* import { Routes, Route } from 'react-router-dom' */}
 import './App.css'
 
 function App() {
-
   return (
     <>
-      
     </>
+    // <Routes>
+    //   <Route path='/' element={}></Route>
+    // </Routes>
   )
 }
 
---
+++ vite-project/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+const homePage = () => {
+    return (
+        <h1>
+            Welcome to the home page
+        </h1>
+    )
+  }
+
+  export default homePage
