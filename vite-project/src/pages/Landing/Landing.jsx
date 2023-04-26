3bcff4fc6388a2b0c7928f79311af4234de52bef
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-04-26 18:37:06 +0200

    comments added

--- vite-project/src/pages/Landing/Landing.jsx
+++ vite-project/src/pages/Landing/Landing.jsx
@@ -8,10 +8,24 @@ const Landing = () => {
   const [isLoading, setIsLoading] = useState(true);
   const controls = useAnimation();
 
+  // READING
+  // useEffect is a hook in React that allows you to perform side effects in your components.
+  // Side effects are actions that affect the state of your application but are not directly related to rendering the UI.
+  // Examples of side effects include fetching data from an API, setting up event listeners, and setting timers.
+
+  // Example:
+  // useEffect(() => {
+  // side effect code here
+  //  }, [dependencies]);
+  //
+
   useEffect(() => {
+    // set a timer to change isLoading state after 2 seconds
     setTimeout(() => {
       setIsLoading(false);
     }, 2000);
+
+    // start an animation with Framer Motion
     controls.start({
       y: [0, 20, 0],
       transition: { duration: 5, repeat: Infinity },
