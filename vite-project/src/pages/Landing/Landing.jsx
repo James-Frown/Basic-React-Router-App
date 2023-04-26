99d22e7f48974d4ce63173f881b8339e788d4792
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-04-26 18:02:00 +0200

    added Loading state

--- vite-project/src/pages/Landing/Landing.jsx
+++ vite-project/src/pages/Landing/Landing.jsx
@@ -1,12 +1,17 @@
 import { motion, useAnimation } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import "./Landing.css";
+import Navbar from "../../components/NavBar";
 
 const Landing = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const controls = useAnimation();
 
   useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
     controls.start({
       y: [0, 20, 0],
       transition: { duration: 5, repeat: Infinity },
@@ -14,38 +19,57 @@ const Landing = () => {
   });
 
   return (
-    <div className="wrapper center">
-      <div className="hero">
-        <motion.div
-          initial={{ x: "-800px" }}
-          animate={{ x: 0 }}
-          transition={{ duration: 1 }}
-          className="left"
+    <>
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "green",
+            fontSize: "50px",
+          }}
         >
-          <div className="heading">
-            <h1>James Brown</h1>
+          Loading...
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <div className="wrapper center">
+            <div className="hero">
+              <motion.div
+                initial={{ x: "-800px" }}
+                animate={{ x: 0 }}
+                transition={{ duration: 1 }}
+                className="left"
+              >
+                <div className="heading">
+                  <h1>James Brown</h1>
+                </div>
+                <div className="text">
+                  <p>Developer && Designer</p>
+                </div>
+              </motion.div>
+              <motion.div
+                initial={{ x: "800px" }}
+                animate={{ x: 0 }}
+                transition={{ duration: 1 }}
+                className="right"
+              >
+                <div className="center wrapper">
+                  <motion.img
+                    animate={controls}
+                    className="image"
+                    src="https://i.ibb.co/dm3KxF1/dummy-img.png"
+                    alt="hero image"
+                  />
+                </div>
+              </motion.div>
+            </div>
           </div>
-          <div className="text">
-            <p>Developer && Designer</p>
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ x: "800px" }}
-          animate={{ x: 0 }}
-          transition={{ duration: 1 }}
-          className="right"
-        >
-          <div className="center wrapper">
-            <motion.img
-              animate={controls}
-              className="image"
-              src="https://i.ibb.co/dm3KxF1/dummy-img.png"
-              alt="hero image"
-            />
-          </div>
-        </motion.div>
-      </div>
-    </div>
+        </>
+      )}
+    </>
   );
 };
 
