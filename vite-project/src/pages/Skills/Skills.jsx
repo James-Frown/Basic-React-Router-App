0df3c6a2c10352f9a2de2907b5e6f552e865b321
James-Frown <21122@ctca.co.za>
2023-05-02 12:09:25 +0200

    trying out the scss

--- vite-project/src/pages/Skills/Skills.jsx
+++ vite-project/src/pages/Skills/Skills.jsx
@@ -2,6 +2,7 @@ import { motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
 
 import Navbar from "../../components/NavBar";
+import "./Special.scss";
 
 const Skills = () => {
   const [isLoading, setIsLoading] = useState(true);
